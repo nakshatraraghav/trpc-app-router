941df745ca0f7137c0505a605ae635d34b7d5b01
nakshatra <combatrickshaw@gmail.com>
2023-11-27 20:18:47 +0530

    (trpc): client setup

---
+++ src/components/providers/trpc-provider.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React, { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { trpc } from "@/lib/trpc/trpc-client";
+
+interface TRPCReactQueryProviderProps {
+  children: React.ReactNode;
+}
+
+export function TRPCReactQueryProvider({
+  children,
+}: TRPCReactQueryProviderProps) {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "http://localhost:3000/api/trpc",
+        }),
+      ],
+    }),
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
+}
